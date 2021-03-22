e8418442aa472d103359329002232749be56a1d3
gabrielaaguilar21 <80728341+gabrielaaguilar21@users.noreply.github.com>
2021-03-21 23:43:20 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.inicio__title', {})
+sr.reveal('.inicio__scroll', {delay: 200})
+sr.reveal('.inicio__img', {origin:'right', delay: 400})
+
+
+sr.reveal('.sobre__img', {delay: 500})
+sr.reveal('.sobre__subtitle', {delay: 300})
+sr.reveal('.sobre__profession', {delay: 400})
+sr.reveal('.sobre__text', {delay: 500})
+sr.reveal('.sobre__social-icon', {delay: 600, interval: 200})
+
+
+sr.reveal('.portafolio__img', {interval: 200})
+
+
+
+
+
